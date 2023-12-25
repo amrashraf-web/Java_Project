b062b3283bc38ddd0953b1d66dd33f9edb4ebc3c
Amr Ashraf <82893114+amrashraf-web@users.noreply.github.com>
2023-12-25 11:56:00 +0200

    Update compile.sh

--- automations/bin/compile.sh
+++ automations/bin/compile.sh
@@ -4,7 +4,7 @@ baseline_cmd="mvn clean package"
 
 if ! test -z "$sonar_url" 
 then
-	sonar_extra_arguments="sonar:sonar -Dsonar.host.url=\"$sonar_url\" -Dsonar.login=sqa_abe2308983442607b86fb259f6ed2ab6621ad6d5 -Dsonar.projectKey=\"$sonar_project_key\" -Dsonar.projectName=\"$sonar_project_name\" -Dsonar.java.coveragePlugin=jacoco -Dsonar.dynamicAnalysis=reuseReports -P sonar -Dsonar.scm.disabled=true -Dsonar.projectVersion=\"$version\""
+	sonar_extra_arguments="sonar:sonar -Dsonar.host.url=\"$sonar_url\" -Dsonar.login=squ_ebc1b937797343e26be57ce1eef2f6f869a2fdb3 -Dsonar.projectKey=\"$sonar_project_key\" -Dsonar.projectName=\"$sonar_project_name\" -Dsonar.java.coveragePlugin=jacoco -Dsonar.dynamicAnalysis=reuseReports -P sonar -Dsonar.scm.disabled=true -Dsonar.projectVersion=\"$version\""
 fi
 
 set -o xtrace
