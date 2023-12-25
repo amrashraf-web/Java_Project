a14f8368f0c8331ecabd0853ae298ddca36064d0
Amr Ashraf <82893114+amrashraf-web@users.noreply.github.com>
2023-12-25 12:27:59 +0200

    Update compile.sh

--- automations/bin/compile.sh
+++ automations/bin/compile.sh
@@ -2,10 +2,7 @@
 
 baseline_cmd="mvn clean package"
 
-if ! test -z "$sonar_url" 
-then
-	sonar_extra_arguments="sonar:sonar -Dsonar.host.url=\"$sonar_url\" -Dsonar.login=squ_ebc1b937797343e26be57ce1eef2f6f869a2fdb3 -Dsonar.projectKey=\"$sonar_project_key\" -Dsonar.projectName=\"$sonar_project_name\"  -Dsonar.projectVersion=\"$version\""
-fi
+sonar_extra_arguments="sonar:sonar -Dsonar.host.url=\"$sonar_url\" -Dsonar.login=squ_ebc1b937797343e26be57ce1eef2f6f869a2fdb3 -Dsonar.projectKey=\"$sonar_project_key\" -Dsonar.projectName=\"$sonar_project_name\"  -Dsonar.projectVersion=\"$version\""
 
 set -o xtrace
 eval $baseline_cmd $maven_extra_arguments $sonar_extra_arguments
