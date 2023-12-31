ff622eecfd21255c6141330242a17812079c446c
Amr Ashraf <82893114+amrashraf-web@users.noreply.github.com>
2023-12-31 12:27:33 +0200

    Update compile.sh

--- automations/bin/compile.sh
+++ automations/bin/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-baseline_cmd="mvn clean package  -U"
+baseline_cmd="mvn clean install  -U"
 
 sonar_extra_arguments="sonar:sonar -Dsonar.host.url=\"$sonar_url\" -Dsonar.login=squ_ebc1b937797343e26be57ce1eef2f6f869a2fdb3 -Dsonar.projectKey=\"$sonar_project_key\" -Dsonar.projectName=\"$sonar_project_name\"  -Dsonar.projectVersion=\"$version\""
 
